1c6d37e452c3f98d76fde4c74ca93337082e94f4
vikaschowdary47 <vikaschowdary47@gmail.com>
2020-10-04 19:34:57 +0530

    form in frontend

--- client/src/components/Expenses.js
+++ client/src/components/Expenses.js
@@ -1,19 +1,23 @@
 import React from "react";
-// import EditIcon from '@material-ui/icons/Edit';
-// import HighlightOffTwoToneIcon from '@material-ui/icons/HighlightOffTwoTone';
+import EditIcon from "@material-ui/icons/Edit";
+import HighlightOffTwoToneIcon from "@material-ui/icons/HighlightOffTwoTone";
 
 const Expenses = () => {
   return (
     <div className="expense">
       <div className="expense-body">
-        <div>Edit</div>
+        <div className="icons">
+          <EditIcon />
+          Edit
+        </div>
         <div>
           <div className="expense-content">
             <div>
-              <div>Date</div>
+              <div className="date mb-1">Date</div>
               <h6>Expense Title</h6>
               <div className="expense-note">
-                Note:<p>Note for expense goes here</p>
+                <span>Note:</span>
+                <p>Note for expense goes here</p>
               </div>
             </div>
             <div className="expense-cost">
@@ -21,133 +25,10 @@ const Expenses = () => {
             </div>
           </div>
         </div>
-        <div>Remove</div>
-      </div>
-      <div className="expense-body">
-        <div>Edit</div>
-        <div>
-          <div className="expense-content">
-            <div>
-              <div>Date</div>
-              <h6>Expense Title</h6>
-              <div className="expense-note">
-                Note:<p>Note for expense goes here</p>
-              </div>
-            </div>
-            <div className="expense-cost">
-              <h4>$5000</h4>
-            </div>
-          </div>
+        <div className="icons">
+          <HighlightOffTwoToneIcon />
+          Remove
         </div>
-        <div>Remove</div>
-      </div>
-      <div className="expense-body">
-        <div>Edit</div>
-        <div>
-          <div className="expense-content">
-            <div>
-              <div>Date</div>
-              <h6>Expense Title</h6>
-              <div className="expense-note">
-                Note:<p>Note for expense goes here</p>
-              </div>
-            </div>
-            <div className="expense-cost">
-              <h4>$5000</h4>
-            </div>
-          </div>
-        </div>
-        <div>Remove</div>
-      </div>
-      <div className="expense-body">
-        <div>Edit</div>
-        <div>
-          <div className="expense-content">
-            <div>
-              <div>Date</div>
-              <h6>Expense Title</h6>
-              <div className="expense-note">
-                Note:<p>Note for expense goes here</p>
-              </div>
-            </div>
-            <div className="expense-cost">
-              <h4>$5000</h4>
-            </div>
-          </div>
-        </div>
-        <div>Remove</div>
-      </div>
-      <div className="expense-body">
-        <div>Edit</div>
-        <div>
-          <div className="expense-content">
-            <div>
-              <div>Date</div>
-              <h6>Expense Title</h6>
-              <div className="expense-note">
-                Note:<p>Note for expense goes here</p>
-              </div>
-            </div>
-            <div className="expense-cost">
-              <h4>$5000</h4>
-            </div>
-          </div>
-        </div>
-        <div>Remove</div>
-      </div>
-      <div className="expense-body">
-        <div>Edit</div>
-        <div>
-          <div className="expense-content">
-            <div>
-              <div>Date</div>
-              <h6>Expense Title</h6>
-              <div className="expense-note">
-                Note:<p>Note for expense goes here</p>
-              </div>
-            </div>
-            <div className="expense-cost">
-              <h4>$5000</h4>
-            </div>
-          </div>
-        </div>
-        <div>Remove</div>
-      </div>
-      <div className="expense-body">
-        <div>Edit</div>
-        <div>
-          <div className="expense-content">
-            <div>
-              <div>Date</div>
-              <h6>Expense Title</h6>
-              <div className="expense-note">
-                Note:<p>Note for expense goes here</p>
-              </div>
-            </div>
-            <div className="expense-cost">
-              <h4>$5000</h4>
-            </div>
-          </div>
-        </div>
-        <div>Remove</div>
-      </div>
-      <div className="expense-body">
-        <div>Edit</div>
-        <div>
-          <div className="expense-content">
-            <div>
-              <div>Date</div>
-              <h6>Expense Title</h6>
-              <div className="expense-note">
-                Note:<p>Note for expense goes here</p>
-              </div>
-            </div>
-            <div className="expense-cost">
-              <h4>$5000</h4>
-            </div>
-          </div>
-        </div>
-        <div>Remove</div>
       </div>
     </div>
   );
